646e84f56842127fd0d036a7e0b113e2c505a5a9
Dexter <Dexter20091991@gmail.com>
2024-03-29 08:09:09 +0200

    done 3 tasks

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,10 @@
-function makeTransaction(quantity, pricePerDroid) {
-  const summ = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${summ} credits!`;
-}
-
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+function slugify(title) {
+  const titleLowerCase = title.toLowerCase();
+  let slug = titleLowerCase.split(' ');
+  return slug.join('-');
+  }
+  console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+  console.log(slugify("English for developer")); // "english-for-developer"
+  console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+  console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+  
